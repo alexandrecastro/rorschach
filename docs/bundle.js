e609cf2283067d6406ba13c3b158501cfa5f023f
aecr <alexandre.castro@gft.com>
2018-06-13 20:11:13 +0200

    Initial commit

--- docs/bundle.js
+++ docs/bundle.js
@@ -26161,7 +26161,11 @@
 	    _react2.default.createElement(
 	      "span",
 	      { className: "highlight" },
-	      "\u2764"
+	      _react2.default.createElement(
+	        "svg",
+	        { "class": "heart", viewBox: "0 0 32 29.6" },
+	        _react2.default.createElement("path", { d: "M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z" })
+	      )
 	    ),
 	    " by Castro"
 	  );
@@ -26323,7 +26327,7 @@
 	          question
 	        );
 	      }
-	      return _react2.default.createElement('img', { src: '/rorschach/images/' + this.props.card.image, alt: '...' });
+	      return _react2.default.createElement('img', { src: '/rorschach/images/' + this.props.card.image });
 	    }
 	  }, {
 	    key: 'renderAnswers',
